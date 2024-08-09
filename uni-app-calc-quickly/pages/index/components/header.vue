941d2aa809a3585b859764a769d9563b25444e80
zhao.liu <zhao.liu@wayz.ai>
2024-08-09 11:28:12 +0800

    :art: 添加图标的显示，同时根据时间展示表情，时间进行小时和分钟的区分。

--- uni-app-calc-quickly/pages/index/components/header.vue
+++ uni-app-calc-quickly/pages/index/components/header.vue
@@ -3,8 +3,17 @@
 		<image class="img-box" mode="aspectFit" src="../../../static/images/home/time.png" alt="" />
 		<view class="right-box">
 			<view class="num-box">
-				<text class="num-time">{{time}}</text>
-				<text class="num-text">分钟</text>
+				<uni-icons custom-prefix="iconfont" :type="mojiIconfont" :color="iconColor" size="50"></uni-icons>
+				<template v-if="time<60">
+					<text class="num-time">{{time}}</text>
+					<text class="num-text">分钟</text>
+				</template>
+				<template v-else>
+					<text class="num-time">{{house}}</text>
+					<text class="num-text">小时</text>
+					<text class="num-time">{{minute}}</text>
+					<text class="num-text">分钟</text>
+				</template>
 			</view>
 			<text class="study-time-text">今日学习时长</text>
 		</view>
@@ -15,15 +24,39 @@
 
 <script setup>
 	import {
+		computed,
 		ref
 	} from 'vue';
 	const time = ref(0)
+	const house = computed(() => {
+		if(time.value >= 60) {
+			return Math.floor(time.value / 60);
+		}
+	})
+	const minute = computed(() => {
+		if(time.value >= 60) {
+			return time.value % 60
+		}
+	})
+	const iconColor = ref("#ff0000")
+	const mojiIconfont = computed(() => {
+		let moji = 'icon-shangxin'
+		if (time.value >= 60 && time.value <= 120) {
+			moji = 'icon-wubiaoqing'
+			iconColor.value = "#ff8400"
+		} else if (time.value > 120) {
+			moji = 'icon-smile'
+			iconColor.value = "#3e7bfa"
+		}
+		return moji
+	})
 </script>
 
 <style lang="scss">
 	.header-box {
 		display: flex;
 		align-items: center;
+
 		.img-box {
 			width: 50%;
 		}
@@ -35,11 +68,11 @@
 		.num-box {
 			display: flex;
 			align-items: center;
-			margin-bottom: 10rpx;
+			margin-bottom: 10px;
 
 			.num-time {
 				font-size: 48rpx;
-				margin-right: 5rpx;
+				margin: 0 5rpx;
 			}
 
 			.num-text {
