8307e16b8ff09642c99620a68ab4f65a040f54ee
zhao.liu <zhao.liu@wayz.ai>
2025-05-14 10:54:08 +0800

    :fire: 更新文件

--- apps/frontend/react-admin-app/vite.config.ts
+++ apps/frontend/react-admin-app/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-  import { PortNumber } from "@pmm/shared"
+import { PortNumber } from "@pmm/shared"
 console.log(PortNumber)
 // https://vite.dev/config/
 export default defineConfig({
--- uni-app/api/modules/article.js
+++
@@ -1,5 +0,0 @@
-import request from ".."
-
-/** 获取所有分类数据 */
-export const articleCategoryApi = params => request.get("article/category/all", params)
-
