d8a935eea5bd37fbcb080d1fc28abc0639e0ec38
zhao.liu <zhao.liu@wayz.ai>
2025-05-15 14:43:56 +0800

    :art: 基本导入工作完成

---
+++ apps/frontend/react-admin-app/src/router/index.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, redirect } from 'react-router'
+import Layout from "@/layout/index"
+import Login from "@/pages/Login/index"
+import Settings from  "@/pages/Settings/index"
+import Dashboard from "@/pages/Dashboard/index"
+// 登录拦截器：所有 protected 路由的 loader 使用它
+const requireAuth = () => {
+  const isLoggedIn = localStorage.getItem('token') === 'ok'
+  if (!isLoggedIn) throw redirect('/login')
+  return null
+}
+
+const routes = [
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/',
+    loader: requireAuth,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />
+      },
+      {
+        path: 'settings',
+        element: <Settings />
+      }
+    ]
+  }
+]
+
+export const router = createBrowserRouter(routes)
