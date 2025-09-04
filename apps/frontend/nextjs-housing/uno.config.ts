0f46cd025ad212f794b4d91db0714c1324df0d92
zhao.liu <zhao.liu@wayz.ai>
2025-09-04 20:19:07 +0800

    feat(frontend): 添加 Next.js 项目基础配置
    - 新建 .gitignore 文件，添加项目相关忽略项
    - 新建 README.md 文件，提供项目启动和学习指南
    - 新建 eslint 配置文件，设置代码规范检查规则
    - 新建 next 配置文件，定义 Next.js 特定配置

---
+++ apps/frontend/nextjs-housing/uno.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "unocss";
+import baseConfig from "@pmm/unocss-config";
+export default defineConfig({
+  ...baseConfig,
+  shortcuts: {
+    "font-color-primary": "text-[--primary-color]",
+    "font-color-base": "text-[#fff]",
+    "border-radius-base": "rounded-[6px]",
+
+    "layout-header-height": "h-[var(--layout-header-height)]",
+    "layout-footer-height": "h-[var(--layout-footer-height)]",
+    "layout-content-min-height":
+      "min-h-[calc(100vh-var(--layout-header-height)-var(--layout-footer-height))]",
+    "layout-header-bg-color": "bg-[#101d37]",
+    "layout-footer-bg-color": "bg-[#101d37]",
+    "layout-bg-color": "bg-[#fff]",
+  },
+});
